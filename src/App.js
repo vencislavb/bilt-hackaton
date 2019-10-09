ed2f271a6034f7cfd58aaeef1baf215ed43f1e60
Ventsislav Buhlev <vbuhlev@gmail.com>
2019-10-09 13:51:52 +0300

    Run refinery service proxy.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import Button from "@material-ui/core/Button";
 import './App.css';
+import refinery from "./service/refinery";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-        <Button color="primary" variant="contained">Hello World</Button>
-    </div>
-  );
+    const onButtonClick = () => {
+        refinery.run(320);
+    };
+
+    const exampleTask = {generation: 0, id: "d495001d-5199-4206-880b-1b85c41a4ce7", status: "queued"};
+
+    return (
+        <div className="App">
+            <header className="App-header">
+            </header>
+            <Button color="primary" variant="contained" onClick={onButtonClick}>Run</Button>
+        </div>
+    );
 }
 
 export default App;
